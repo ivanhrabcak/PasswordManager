5c00558b74f3c02d23e1edbd614e026aa8ae340e
Ivan <ivan@JCM800.local>
2020-03-09 13:41:12 +0100

    json parse data in PasswordManager

--- app/src/main/java/com/ivik/passwordmanager/PasswordManager.java
+++ app/src/main/java/com/ivik/passwordmanager/PasswordManager.java
@@ -52,36 +52,23 @@ public class PasswordManager {
         }
     }
 
-    public static String hashString(String input) // Copied
-    {
+    public static String hashString(String input) {
         if (input == null || input.equals("")) {
             return " ";
         }
         try {
-            // getInstance() method is called with algorithm SHA-512
             MessageDigest md = MessageDigest.getInstance("SHA-512");
-
-            // digest() method is called
-            // to calculate message digest of the input string
-            // returned as array of byte
             byte[] messageDigest = md.digest(input.getBytes());
 
-            // Convert byte array into signum representation
             BigInteger no = new BigInteger(1, messageDigest);
-
-            // Convert message digest into hex value
             String hashtext = no.toString(16);
 
-            // Add preceding 0s to make it 32 bit
             while (hashtext.length() < 32) {
                 hashtext = "0" + hashtext;
             }
 
-            // return the HashText
             return hashtext;
         }
-
-        // For specifying wrong message digest algorithms
         catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
@@ -153,6 +140,10 @@ public class PasswordManager {
         return null;
     }
 
+    public void parseAccountsFromJsonData(String data) {
+        passwords = new JSONObject(data);
+    }
+    
     public String getJsonData() {
         return passwords.toString();
     }
